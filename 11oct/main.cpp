26523fe8d8c7e8795da6e4397cd2a1b911161cf7
glnskkh <glnskkh@vk.com>
2022-10-11 09:59:31 +0500

    first part

--- 11oct/main.cpp
+++ 11oct/main.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello world!" << endl;
+  // Example
+  // int arr[3]; - array with len 3
+
+  // int a[3] = {0}; // All elements are zeroes
+  // int b[3] = {1,
+  //             2,
+  //             3};
+  // int c[3]{1, 2, 4};
+  // int d[] = {12, 24};
+
+  // Compile-time alloc mem for stack(!)
+  // Stack: fast but sized
+
+  // constexpr - вычисляются во время компиляции
+
+  // typedef int[3] V3;
+
+  const int N = 3;
+  int arr[N];
+
+  // int len = sizeof(arr) / sizeof(arr[0]); // or size(arr)
+
+  for (int i = 0; i < size(arr); ++i)
+    arr[i] = i;
+
+  for (int el : arr)
+    cout << el << endl;
 }
