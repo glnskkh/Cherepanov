f272bb7e2a02ec396b10307e47da9453fa5072ff
glnskkh <glnskkh@vk.com>
2022-10-25 10:06:30 +0500

    make matrix

---
+++ 25oct/matrix.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const char filename[] = "matrix.txt";
+
+  ifstream file(filename);
+
+  if (!file) {
+    cerr << "Error while opening!";
+    return -1;
+  }
+
+  int n, m;
+
+  file >> n >> m;
+
+  int matrix[n][m];
+
+  int index = 0;
+  while (!file.eof()) {
+    string s;
+
+    file >> s;
+
+    int scalar = stoi(s);
+
+    matrix[index / n][index % n] = scalar;
+    ++index;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  file.close();
+}
