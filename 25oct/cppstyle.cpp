a3e5008994bc481add89858fdf073bd77b9b1959
glnskkh <glnskkh@vk.com>
2022-10-25 09:51:52 +0500

    write

--- 25oct/cppstyle.cpp
+++ 25oct/cppstyle.cpp
@@ -6,31 +6,17 @@ using namespace std;
 #define BUF_MAX 64
 
 int main() {
-  const char filename[] = "new.txt";
+  const char filename[] = "hello.txt";
 
-  FILE *file = fopen(filename, "r");
+  FILE *file = fopen(filename, "w");
 
   if (file == NULL) {
     cerr << "Error reading file";
     return -1;
   }
 
-  fseek(file, 0, SEEK_END); // Move cursor
-
-  auto size = ftell(file) - 1; // Size of buffer before cursor
-
-  char ch;
-  while (size >= 0) {
-    fseek(file, size, SEEK_SET);
-
-    ch = fgetc(file);
-
-    // fseek(file, -1, SEEK_CUR);
-
-    cout << ch;
-
-    --size;
-  }
+  char string[] = "Hello!!!\n";
+  fputs(string, file);
 
   fclose(file);
 }
