0d73fd4090253be8288bd184447db1ff27e35887
glnskkh <glnskkh@vk.com>
2022-10-25 09:56:37 +0500

    init

---
+++ 25oct/object.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+#define MAX_BUF 64
+
+int main() {
+  const char filename[] = "numbers.txt";
+
+  ifstream file(filename);
+
+  if (!file) {
+    cerr << "Error while opening!";
+    return -1;
+  }
+
+  char buffer[MAX_BUF];
+  while (!file.eof()) {
+    file.getline(buffer, MAX_BUF);
+
+    cout << buffer;
+  }
+
+  file.close();
+}
