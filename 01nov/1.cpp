0ad9c9d843056c75ab58ebdfed493b85dbd7489e
glnskkh <glnskkh@vk.com>
2022-11-01 09:42:00 +0500

    2

--- 01nov/1.cpp
+++ 01nov/1.cpp
@@ -5,12 +5,11 @@ using namespace std;
 const int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int foundedNumbers[10];
 
-void SelectNumbers(int i) {
-  static int start = 0;
-
-  foundedNumbers[start++] = numbers[i];
-
-  start %= 10;
+void SelectNumbers(int x, int n = 1) {
+  for (int i = 0; i < 10; ++i)
+    if (numbers[i] == x)
+      for (int j = 0; j < n && j + i < 10; ++j)
+        foundedNumbers[j] = numbers[i + j];
 }
 
 bool Contains(int x) {
@@ -24,8 +23,16 @@ bool Contains(int x) {
 int Square(int x) { return x * x; }
 
 int main() {
-  SelectNumbers(1);
-  SelectNumbers(2);
+  SelectNumbers(1, 2);
+
+  for (int found : foundedNumbers)
+    cout << found << endl;
+
+  SelectNumbers(2, 3);
+
+  for (int found : foundedNumbers)
+    cout << found << endl;
+
   SelectNumbers(9);
 
   for (int found : foundedNumbers)
